5991a3d4ac263090f1c8c297645fed70a6e52ad5
Isaiah N. <rinriukato@gmail.com>
2024-12-26 12:01:47 -0500

    changed the UI arrangement for mobile apps, updated the object model

--- Assets/Scripts/ZoneDetection.cs
+++ Assets/Scripts/ZoneDetection.cs
@@ -1,11 +1,34 @@
 using UnityEngine;
 using System.Collections.Generic;
+using TMPro;
 
 public class ZoneDetection : MonoBehaviour
 {
 
+    public TextMeshProUGUI textMeshPro;
     void OnTriggerEnter(Collider other) 
     {
-        Debug.Log("Detected Object: " + other);
+        string name = other.gameObject.name;
+        string zoneName = "None";
+
+        if (name == "DangerZoneGreen") 
+        {
+            zoneName = "Green";
+        }
+        else if (name == "DangerZoneYellow")
+        {
+            zoneName = "Yellow";
+        }
+        else if (name == "DangerZoneOrange")
+        {
+            zoneName = "Orange";
+        }
+        else if (name == "DangerZoneRed")
+        {
+            zoneName = "Red";
+        }
+
+        textMeshPro.text = "Zone: " + zoneName;
     }
+
 }
